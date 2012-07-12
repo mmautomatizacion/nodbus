56d21b5419d36cdd48575fc352827fe1a7f7d66c
luis <luis@192.168.1.128>
2012-07-12 12:56:51 +0200

    Versión inicial

---
+++ index.js
@@ -0,0 +1,6 @@
+/* 
+ * MM SoftGestion por Luis Gómez se encuentra bajo una Licencia Creative Commons
+ * No se permite un uso comercial de la obra original ni la generación de obras derivadas.
+ */
+
+module.exports = require('./lib/dispositivo.js');
